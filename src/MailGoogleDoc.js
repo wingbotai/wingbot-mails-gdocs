86669952529401381bfc0e170be7e3481dd2867d
Ondřej Vereš <ondra.veres@gmail.com>
2024-09-10 15:03:18 +0200

    fixed eslintrc

--- src/MailGoogleDoc.js
+++ src/MailGoogleDoc.js
@@ -1,12 +1,14 @@
 'use strict';
 
 const { JWT } = require('google-auth-library');
+// eslint-disable-next-line camelcase
 const { drive_v3 } = require('@googleapis/drive');
 const cheerio = require('cheerio');
 const Handlebars = require('handlebars');
 const { convert } = require('html-to-text');
 
 const fs = require('fs');
+const path = require('path');
 /**
  * @typedef {object} TestCaseStep
  * @prop {number} step
@@ -45,13 +47,9 @@ const fs = require('fs');
  * Google sheets storage for test cases
  */
 
-Handlebars.registerHelper('gt', function (a, b) {
-    return a > b;
-});
+Handlebars.registerHelper('gt', (a, b) => a > b);
 
-Handlebars.registerHelper('createLink', function (url, text) {
-    return new Handlebars.SafeString(`<a href="${url}">${text}</a>`);
-});
+Handlebars.registerHelper('createLink', (url, text) => new Handlebars.SafeString(`<a href="${url}">${text}</a>`));
 
 class MailGoogleDoc {
 
@@ -68,22 +66,21 @@ class MailGoogleDoc {
                 'https://www.googleapis.com/auth/drive.readonly'
             ]
         });
+        // eslint-disable-next-line camelcase
         this._googleDrive = new drive_v3.Drive({ auth });
-        this._docId = docId
+        this._docId = docId;
         this._googleToken = googleToken;
         this._$ = null;
         this._htmlToTextConverterOptions = {
-            wordwrap: false,
-          };
+            wordwrap: false
+        };
+        this._subdirectory = path.join(__dirname, 'mail_templates');
+
+        this._filePath = path.join(this._subdirectory, `${this._docId}.html`);
     }
 
-    /**
-     * @param {('cs'|'en'|'sk'|'pl'|'de')} locale
-     * @param {object} dataForHandlebarsTemplating
-     * @returns {Promise<MailData>}
-     */
-   async getMailData(locale, dataForHandlebarsTemplating)
-   {
+    async downloadMailTemplate () {
+
         const htmlRes = await this._googleDrive.files.export({
             fileId: this._docId,
             mimeType: 'text/html'
@@ -91,40 +88,57 @@ class MailGoogleDoc {
 
         /** @type {string} */
         // @ts-ignore
-        const html =  htmlRes.data;
+        const html = htmlRes.data;
+
+        if (!fs.existsSync(this._subdirectory)) {
+            fs.mkdirSync(this._subdirectory);
+        }
 
         // Save the HTML content to a file
-        fs.writeFile('output.html', html, (err) => {
+        fs.writeFile(this._filePath, html, (err) => {
             if (err) {
-                console.error('Error writing to file', err);
-            } else {
-                console.log('HTML content saved to output.html');
+                throw new Error(`Error saving mail template: ${err.message}`);
             }
         });
+        return this._filePath;
+    }
+
+    /**
+     * @param {('cs'|'en'|'sk'|'pl'|'de')} locale
+     * @param {object} dataForHandlebarsTemplating
+     * @returns {Promise<MailData>}
+     */
+    async getMailData (locale, dataForHandlebarsTemplating) {
+
+        const html = fs.readFileSync(this._filePath, 'utf8');
 
         this._$ = cheerio.load(html);
         // Define the template strings
-        const to = this._findValueByKey("Komu")?.split(",")?.map((address) => address?.trim()?.toLowerCase());
-        const cc = this._findValueByKey("Kopie")?.split(",")?.map((address) => address?.trim()?.toLowerCase());
-        const bcc = this._findValueByKey("Skrytá kopie")?.split(",")?.map((address) => address?.trim()?.toLowerCase());
+        const to = this._findValueByKey('Komu')?.split(',')?.map((address) => address?.trim()?.toLowerCase());
+        const cc = this._findValueByKey('Kopie')?.split(',')?.map((address) => address?.trim()?.toLowerCase());
+        const bcc = this._findValueByKey('Skrytá kopie')?.split(',')?.map((address) => address?.trim()?.toLowerCase());
         const subjectTemplate = this._findValueByKey(`Předmět ${locale}`);
         const messageHtmlTemplate = `${this._findValueByKey(`Zpráva ${locale}`, true)}`;
         // @ts-ignore
         const subject = Handlebars.compile(subjectTemplate)(dataForHandlebarsTemplating);
-        const messageHtml = this.stripStylesExceptFontWeight(Handlebars.compile(messageHtmlTemplate)(dataForHandlebarsTemplating));
+        const messageHtml = this.stripStylesExceptFontWeight(
+            Handlebars.compile(messageHtmlTemplate)(dataForHandlebarsTemplating)
+        );
         // @ts-ignore
         const messagePlainText = convert(messageHtml, this._htmlToTextConverterOptions);
 
-        return {to, cc, bcc, subject, messagePlainText, messageHtml}
+        return {
+            to, cc, bcc, subject, messagePlainText, messageHtml
+        };
+    }
 
-   }
-
-   /**
+    /**
      * Function to strip away all style information except font-weight.
+     *
      * @param {string} html - The HTML string to process.
      * @returns {string} - The processed HTML string.
      */
-    stripStylesExceptFontWeight(html) {
+    stripStylesExceptFontWeight (html) {
         const $ = cheerio.load(html);
 
         $('[style]').each((index, element) => {
@@ -142,7 +156,6 @@ class MailGoogleDoc {
                 $element.replaceWith('<br>');
             }
         });
-    
 
         return $.html();
     }
@@ -150,36 +163,54 @@ class MailGoogleDoc {
     /**
      * Function to find the value in the second column for a given key in the first column.
      * If the row has only one column, the value is in the next row with a single column.
+     *
      * @param {string} key - The key to search for in the first column.
      * @param {boolean} [returnHtml=false] - Whether to return the inner HTML instead of text.
+     * @param {boolean} withNewLines
      * @returns {string|null} - The value in the second column, or null if the key is not found.
      */
-    _findValueByKey(key, returnHtml = false, withNewLines = false) {
+    _findValueByKey (key, returnHtml = false, withNewLines = false) {
         let value = null;
         this._$('table tr').each((index, element) => {
-            const firstColumnText = this._$(element).find('td').eq(0).text().trim();
+            const firstColumnText = this._$(element).find('td').eq(0).text()
+                .trim();
             const secondColumn = this._$(element).find('td').eq(1);
-            
+
             if (firstColumnText === key) {
                 if (secondColumn.length > 0) {
                     // @ts-ignore
-                    value = returnHtml ? secondColumn.html().trim() : (withNewLines ? convert(secondColumn.html(), this._htmlToTextConverterOptions) : secondColumn.text().trim());
+                    if (returnHtml) {
+                        value = secondColumn.html().trim();
+                    } else if (withNewLines) {
+                        // @ts-ignore
+                        value = convert(secondColumn.html(), this._htmlToTextConverterOptions);
+                    } else {
+                        value = secondColumn.text().trim();
+                    }
                 } else {
                     const nextRow = this._$('table tr').eq(index + 1);
                     const nextRowSingleColumn = nextRow.find('td').eq(0);
                     if (nextRowSingleColumn.length === 1) {
-                        // @ts-ignore
-                        value = returnHtml ? nextRowSingleColumn.html().trim() : (withNewLines ? convert(nextRowSingleColumn.html(), this._htmlToTextConverterOptions) : nextRowSingleColumn.text().trim());
+
+                        if (returnHtml) {
+                            value = nextRowSingleColumn.html().trim();
+                        } else if (withNewLines) {
+                            value = convert(
+                                nextRowSingleColumn.html(),
+                                // @ts-ignore
+                                this._htmlToTextConverterOptions
+                            );
+                        } else {
+                            value = nextRowSingleColumn.text().trim();
+                        }
                     }
                 }
-                return false;
+                return false; // breaks cycle
             }
+            return true;
         });
         return value;
     }
 }
-
-
-
 
 module.exports = MailGoogleDoc;
